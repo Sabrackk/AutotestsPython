d00d146d936df77ca4bc450621e8d26d287b89fd
Sabrackk <Vadim.khisamutdinov1@yandex.ru>
2025-05-07 22:21:15 +0500

    Add files via upload

--- ProjectsPython/tests/test_pokemon.py
+++ ProjectsPython/tests/test_pokemon.py
@@ -10,6 +10,10 @@ def test_status_code():
     response = requests.get(url = f'{URL}/trainers')
     assert response.status_code == 200
 
-def test_name_my_trainer():
+def test_name_my_trainer1():
     response_get = requests.get(url = f'{URL}/trainers/29404')
     assert response_get.json()['trainer_name'] == 'Аврора'
+
+def test_name_my_trainer2():
+    response_get = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER_ID})
+    assert response_get.json()['data'][0]['trainer_name'] == 'Аврора'
